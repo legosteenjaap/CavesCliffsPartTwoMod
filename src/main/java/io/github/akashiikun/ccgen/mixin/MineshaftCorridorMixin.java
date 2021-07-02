b303e30546b5ca5b49520cfd4f4f621ea4430237
Akashii Kun <44606942+AkashiiKun@users.noreply.github.com>
2021-07-02 15:40:48 +0100

    Change System to CaveBiomeApi's Source Code

--- src/main/java/io/github/akashiikun/ccgen/mixin/MineshaftCorridorMixin.java
+++ src/main/java/io/github/akashiikun/ccgen/mixin/MineshaftCorridorMixin.java
@@ -1,13 +1,5 @@
 package io.github.akashiikun.ccgen.mixin;
 
-import java.util.Random;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import net.minecraft.structure.MineshaftGenerator;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
@@ -15,9 +7,16 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
 
 @Mixin(MineshaftGenerator.MineshaftCorridor.class)
-public class MineshaftCorridorMixin {
+class MineshaftCorridorMixin {
 	@Shadow private boolean hasSpawner;
 	
 	/**
--- src/main/java/io/github/akashiikun/ccgen/mixin/MixinSmallDripstoneFeature.java
+++ src/main/java/io/github/akashiikun/ccgen/mixin/MixinSmallDripstoneFeature.java
@@ -1,17 +1,15 @@
 package io.github.akashiikun.ccgen.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.DripstoneClusterFeatureConfig;
 import net.minecraft.world.gen.feature.SmallDripstoneFeature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SmallDripstoneFeature.class)
 public class MixinSmallDripstoneFeature {
@@ -20,7 +18,7 @@ public class MixinSmallDripstoneFeature {
 	 * @author Legosteenjaap
 	 */
 	
-	@Inject(method = "generate", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "generate(Lnet/minecraft/world/gen/feature/util/FeatureContext;)Z", at = @At("HEAD"), cancellable = true)
 	public void generate(FeatureContext<DripstoneClusterFeatureConfig> context, CallbackInfoReturnable<Boolean> cir) {
 		StructureWorldAccess world = context.getWorld();
 		BlockPos pos = context.getOrigin();
